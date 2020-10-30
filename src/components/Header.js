e03346e37b772a3ff1f5d7fed2e3a6e399ff52eb
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-10-30 10:50:51 -0300

    hello world!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
-let React = require('react');
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-boostrap/Container';
 
-function Header(){
-    return <header></header>
+
+function Header(props){
+    return  <Jumbotron fluid>
+                <Container>
+                    <h1>{props.titulo}</h1>
+                </Container>
+            </Jumbotron>
 }
+
+export default Header;
